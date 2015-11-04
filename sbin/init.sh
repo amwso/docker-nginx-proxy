688b5c3c4848064596ac49e3445b9c163ae161da
amwso <trixism@qq.com>
2015-11-04 13:14:58 +0800

    update

--- sbin/init.sh
+++ sbin/init.sh
@@ -7,6 +7,7 @@ CONF_PATH=$DATA_PATH/conf
 TEMP_PATH=$DATA_PATH/tmp
 ARACRON_PATH=$DATA_PATH/var/spool/anacron
 LOGROTATE_PATH=$DATA_PATH/var/lib/logrotate
+NGINX_CACHE_PATH=$DATA_PATH/var/cache/nginx
 
 
 check_dir () {
@@ -15,14 +16,28 @@ check_dir () {
 	[[ ! -d $LOG_FILE_PATH/nginx ]] && mkdir -p $LOG_FILE_PATH/nginx
 	[[ ! -d $ARACRON_PATH ]] && mkdir -p $ARACRON_PATH
 	[[ ! -d $LOGROTATE_PATH ]] && mkdir -p $LOGROTATE_PATH
+	[[ ! -d $NGINX_CACHE_PATH ]] && mkdir -p $NGINX_CACHE_PATH
 	[[ ! -d $CONF_PATH ]] && cp -rf /root/template/conf $CONF_PATH
 	[[ ! -f $CONF_PATH/supervisor_service.conf ]] && cp -f /root/template/conf/supervisor_service.conf $CONF_PATH/supervisor_service.conf
 	[[ ! -d $TEMP_PATH ]] && mkdir -p $TEMP_PATH
 	chmod 1777 $TEMP_PATH
 }
 
+setup_upsteam () {
+        if [ "x$NGINX_UPSTREAM" == "x" ] ; then
+                echo "no upstream, exit."
+		exit 1
+	elif [[ ! $NGINX_UPSTREAM =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(:[0-9]+)?$ ]] ; then
+		echo "bad upstream, exit."
+		exit 1
+        else
+		echo "upstream http_backend { server $NGINX_UPSTREAM; keepalive 16; }" > $CONF_PATH/nginx/nginx_upstream.conf
+        fi
+}
 
 check_dir
+setup_upsteam
+
 # load crontab
 crontab $CONF_PATH/crontab.root
 # run anacron once
